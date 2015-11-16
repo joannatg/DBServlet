e2241d6899b350edb75b826468ee13736289d874
Joanna Georgescu <jptgeorgescu@gmail.com>
2015-11-16 11:37:10 -0500

    Commiting Calculator

---
+++ Calculator/src/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package calculator;
+import javax.jws.WebService;
+
+@WebService
+public class Calculator {
+	public void constructor() {
+		
+	}
+	
+	
+	
+public String Hello (String s) {
+	return"Hello " + s;
+}
+}
